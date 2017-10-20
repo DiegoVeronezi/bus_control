0b2e3a672cd9793ad9f20eaeb74aeea48ba9fb0a
DiegoVeronezi <diegoveronezi70@gmail.com>
2017-10-20 18:23:10 -0200

    melhoramentos do controle de onibus

--- src/main/java/br/com/diegoveronezi/buscontrol/Main.java
+++ src/main/java/br/com/diegoveronezi/buscontrol/Main.java
@@ -2,12 +2,23 @@ package br.com.diegoveronezi.buscontrol;
 
 import br.com.ConexaoBanco.ConexaoMySQL;
 
+import java.sql.Time;
+import java.time.LocalTime;
+
+
 public class Main {
 
     public static void main(String[] args) {
 
-        ConexaoMySQL.getConexaoMySQL();
-        System.out.println(ConexaoMySQL.status);
+        //TESTE DA CONEXÃO DO BD
+        /*ConexaoMySQL.getConexaoMySQL();
+        System.out.println(ConexaoMySQL.status);*/
+
+        LocalTime now = LocalTime.now();
+
+        Time time = Time.valueOf(now);
+
+        System.out.println("Horário atual:" + time);
 
     }
 
